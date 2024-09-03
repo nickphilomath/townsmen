b22f8737eac7af1964922ee010504c7ba14e2dd4
Nick <nickphilomath@gmail.com>
2024-09-03 23:38:33 +0500

    render a sprite from tiles; rename README

--- src/main.cpp
+++ src/main.cpp
@@ -2,9 +2,16 @@
 
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
-    sf::CircleShape shape(100.f);
-    shape.setFillColor(sf::Color::Green);
+    sf::RenderWindow window(sf::VideoMode(1200, 800), "SFML works!");
+
+    sf::Texture texture;
+    texture.loadFromFile("resources/images-content/tilesets/tiles_summer_1.png");
+
+    sf::Sprite sprite;
+    sprite.setTexture(texture);
+    // <sprite n="bridge_1x1_ggww_10001000_0_g" x="2084" y="322" w="217" h="140" pX="0.5" pY="0.5" oX="3" oY="20" oW="256" oH="160"/>
+    sprite.setTextureRect(sf::IntRect(2084, 322, 217, 140));
+    // sprite.setPosition(0, 0);
 
     while (window.isOpen())
     {
@@ -16,7 +23,7 @@ int main()
         }
 
         window.clear();
-        window.draw(shape);
+        window.draw(sprite);
         window.display();
     }
 
